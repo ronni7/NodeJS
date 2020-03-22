d4b4fc37e848ab22c5fcefed01a65faf285a5cbb
Ronni <aomab77@gmail.com>
2020-03-22 11:49:57 +0100

    Adds GET image endpoint, changes upload method, now filename is a generated UUID. Removes unusued test endpoints

--- routes/sampleHelloWorld.js
+++ routes/sampleHelloWorld.js
@@ -1,26 +1,28 @@
+const uuidv1 = require('uuid').v1;
 const express = require('express');
 const router = express.Router();
 const multer = require("multer");
 const path = require('path');
 const fs = require('fs');
-module.exports = router;
-router.get('/hello', function (req, res, next) {
-    res.send({name: 'Johnny', age: 24});
-});
-
-router.get('/helloWithParams', function (req, res, next) {
-    res.send({responseBody: req.query});
-});
-
-router.post('/post', function (req, res, next) {
-    res.send({responseBody: req && req.body ? req.body : 'NotFound'});
-});
-
-router.post('/uploadImage', function (req, res, next) {
-    req.send('OK');
-});
 const upload = multer({
     dest: "/storedFiles/images"
+});
+module.exports = router;
+
+router.get('/image', function (req, res, next) {
+    if (!req.query.filename) {
+        res.status(400).json({error: 'Please provide a filename'}).send();
+    }
+    const filename = req.query.filename;
+    const path = __dirname + "/../storedFiles/images/" + filename + ".png";
+    fs.readFile(path, (err, data) => {
+        if (err) {
+            console.error(err);
+            res.status(404).json({error: "File not found"}).send();
+        }
+        res.send(data);
+    })
+
 });
 
 router.post(
@@ -28,16 +30,18 @@ router.post(
     upload.single(""),
     (req, res) => {
         const tempPath = req.file.path;
-        const targetPath = path.join(__dirname, "/../storedFiles/images/image.png");
+        const uuid = uuidv1();
+        const targetPath = path.join(__dirname, "/../storedFiles/images/" + uuid + ".png");
         if (!req.file) {
             return res.status(401).json({error: 'Please provide an image'});
         }
         fs.readFile(tempPath, function (err, data) {
             fs.writeFile(targetPath, data, function (err) {
-                console.error(err);
+                if (err)
+                    console.error(err);
             })
         });
-        res.status(200).json({name: req.file.filename});
+        res.status(200).json({filename: uuid});
         res.send();
-           }
+    }
 );
