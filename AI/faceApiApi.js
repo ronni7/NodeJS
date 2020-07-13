2e55cfda63c5d076c2c3e82f0ccc43a180264f04
Ronni <aomab77@gmail.com>
2020-07-13 18:57:42 +0200

    Added multiple face detection with example analysis file, code refactor

--- AI/faceApiApi.js
+++ AI/faceApiApi.js
@@ -17,19 +17,35 @@ module.exports = {
                     const image = new Image();
                     image.src = sourceImg;
                     detection = await faceapi.detectSingleFace(image).withFaceExpressions().withAgeAndGender();
-                    let result = {};
                     if (detection) {
-                        result.emotion = {key: 'undefined', value: 0.6};
-                        if (detection.hasOwnProperty('expressions')) {
-                            for (const [key, value] of Object.entries(detection.expressions)) {
-                                if (value > result.emotion.value) {
-                                    result.emotion = {key, value};
-                                }
-                            }
-                        }
-                        if (detection.hasOwnProperty('genderProbability') && detection.hasOwnProperty('gender')) result.gender = detection.genderProbability > 0.6 ? detection.gender : 'unknown';
-                        if (detection.hasOwnProperty('age') && detection.hasOwnProperty('age')) result.age = Number.isNaN(detection.age) ? -1 : Math.round(detection.age);
+                        let result = createDetectionResult(detection);
                         fs.writeFileSync(targetPath.split('.')[0] + '.json', JSON.stringify(result));
+                    }
+                } catch (error) {
+                    console.error(error);
+                }
+            })();
+        },
+    detectMultipleWithAgeGender:
+        function (sourceImg, targetPath) {
+            let detection = {};
+            (async () => {
+                await faceapi.nets.ssdMobilenetv1.loadFromDisk('./AI/weights');
+                await faceapi.nets.faceLandmark68Net.loadFromDisk('./AI/weights');
+                await faceapi.nets.faceExpressionNet.loadFromDisk('./AI/weights');
+                await faceapi.nets.ageGenderNet.loadFromDisk('./AI/weights');
+                try {
+                    const image = new Image();
+                    image.src = sourceImg;
+                    detection = await faceapi.detectAllFaces(image).withFaceExpressions().withAgeAndGender();
+                    let resultArray = [];
+                    if (detection && Array.isArray(detection)) {
+                        for (const singleDetection of detection) {
+                            let result = createDetectionResult(singleDetection);
+                            resultArray.push(result);
+                        }
+                        if (resultArray && resultArray.length > 0)
+                            fs.writeFileSync(targetPath.split('.')[0] + '.json', JSON.stringify(resultArray));
                     }
                 } catch (error) {
                     console.error(error);
@@ -38,3 +54,14 @@ module.exports = {
         }
 };
 
+function createDetectionResult(singleDetection) {
+    let result = {};
+    result.emotion = {key: 'undefined', value: 0.6};
+    if (singleDetection.hasOwnProperty('expressions'))
+        for (const [key, value] of Object.entries(singleDetection.expressions))
+            if (value > result.emotion.value)
+                result.emotion = {key, value};
+    if (singleDetection.hasOwnProperty('genderProbability') && singleDetection.hasOwnProperty('gender')) result.gender = singleDetection.genderProbability > 0.6 ? singleDetection.gender : 'unknown';
+    if (singleDetection.hasOwnProperty('age') && singleDetection.hasOwnProperty('age')) result.age = Number.isNaN(singleDetection.age) ? -1 : Math.round(singleDetection.age);
+    return result;
+}
